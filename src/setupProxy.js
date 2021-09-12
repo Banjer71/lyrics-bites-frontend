5c4415c8e595eb6cee6283902125d2eca20dcceb
Banjer71 <davgio7@gmail.com>
2021-09-12 11:24:10 +0200

    test branch

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,17 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
-	app.use(
-		createProxyMiddleware('/ws/1.1', {
-			target: 'https://api.musixmatch.com',
-			changeOrigin: true
-		})
-	);
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/ws/1.1", {
+      target: "https://api.musixmatch.com",
+      changeOrigin: true,
+    })
+  );
 
-	app.use(
-		createProxyMiddleware('/2.0', {
-			target: 'http://ws.audioscrobbler.com',
-			changeOrigin: true
-		})
-	);
+  app.use(
+    createProxyMiddleware("/2.0", {
+      target: "http://ws.audioscrobbler.com",
+      changeOrigin: true,
+    })
+  );
 };
